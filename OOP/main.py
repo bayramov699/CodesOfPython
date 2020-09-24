2e5ee13efa92703463ab7d393393ecf9e10c2c61
roma <bayramov699@gmail.com>
2020-09-24 19:01:36 +0400

    oop3

--- OOP/main.py
+++ OOP/main.py
@@ -5,18 +5,20 @@ class Person:
         self.name=name
         self.surname=surname
         self.person_type=person_type
-      
-
+    def __str__(self):
+        return
+        
+class Alive:
     def greeting(self):
         return f"Hello, Dear {self.name}. Welcome to my new program"
-
-class Family(Person):
+    
+class Family(Person, Alive):
     pass
     
-class Friend(Person):
+class Friend(Person, Alive):
     pass
 
-class Collegue(Person):
+class Collegue(Person, Alive):
     pass
 
 
@@ -40,5 +42,12 @@ elif person_type=='3':
     prs=Collegue(name = name, surname = surname, person_type='collegue')
     address_book['collegues'].append(vars(prs))
 
+rauf= Family(name='Rauf', surname='Bayramov', person_type='family')
+
 #print(Person.greeting())
 print(address_book)
+print(Family.greeting(rauf))
+print(issubclass(Family, list))
+print(isinstance(Family, tuple))
+print(Friend.__mro__)
+print(Friend.mro())
